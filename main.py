76fc2def059de963034566cf4d79b02bda3eb49e
visiongem <393507488@qq.com>
2021-03-28 17:32:55 +0800

    233

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+import os
+import re
+import pytz
+from datetime import datetime
+
+def main():
+
+    # 替换 ---start--- 到 ---end--- 之间的内容
+    # pytz.timezone('Asia/Shanghai')).strftime('%Y年%m月%d日%H时M分')
+    fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+    insert_info = "---start---\n\n## Yane.zZ的每日更新(" + "更新时间:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') + " | 本部分通过Github Actions抓取RSS自动更新, 无意中实现了自动刷绿墙...)" +"\n" + "---end---"
+    # 获取README.md内容
+    with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
+        readme_md_content = f.read()
+
+    print(insert_info)
+
+    new_readme_md_content = re.sub(r'---start---(.|\n)*---end---', insert_info, readme_md_content)
+
+    with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
+        f.write(new_readme_md_content)
+
+
+main()
